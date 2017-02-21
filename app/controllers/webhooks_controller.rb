48e67d76f212bb5fd78efd963f786914e3590be1
Nikki Veen <nikkiveen@gmail.com>
2017-02-21 11:27:42 -0600

    added sad path for webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,19 +1,27 @@
 class WebhooksController < ApplicationController
   def twilio
     @lead = Lead.find_by(phone: params["from"])
-    if @lead.update(active: false)
-      render json: @lead
+    if @lead
+      if @lead.update(active: false)
+        render json: @lead
+      else
+        render json: @lead.errors, status: :unprocessable_entity
+      end
     else
-      render json: @lead.errors, status: :unprocessable_entity
+      head :no_content
     end
   end
 
   def sendgrid
     @lead = Lead.find_by(email: params["from"])
-    if @lead.update(active: false)
-      render json: @lead
+    if @lead
+      if @lead.update(active: false)
+        render json: @lead
+      else
+        render json: @lead.errors, status: :unprocessable_entity
+      end
     else
-      render json: @lead.errors, status: :unprocessable_entity
+      head :no_content
     end
   end
 end
